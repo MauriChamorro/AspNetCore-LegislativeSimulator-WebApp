ebd93f4a5ef0bbbbfd968b2d98f8d8d8373d4b0d
MauriChamorro <mmchamoo@gmail.com>
2026-04-13 16:41:58 -0300

    person model

---
+++ Models/Person.cs
@@ -0,0 +1,9 @@
+namespace WebAppMVC.Models;
+
+public class Person
+{
+    public int PersonId { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required int Age { get; set; }
+}
