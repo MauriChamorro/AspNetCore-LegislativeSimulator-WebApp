0745a016b06274bf8d98b7e95403332645842d59
MauriChamorro <mmchamoo@gmail.com>
2026-04-13 17:09:34 -0300

    pre db config

---
+++ Contexts/AppDbContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using WebAppMVC.Models;
+
+namespace WebAppMVC.Contexts;
+
+// class for create a "session" with the DB
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+    {
+        //initialize context
+        //set configs --> options
+        //inyections
+        
+        
+    }
+    
+    //DBSet, clase de EF que representa una COLECCION de entidades en el contexto
+    //EF va usar esta case para LEER Y ESCRIBIR en la BASE DE DATOS
+    public DbSet<Person> Persons { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebAppMVC.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("SqliteConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
 
 var app = builder.Build();
 
