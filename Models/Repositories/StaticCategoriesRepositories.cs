37dd0f9495cab6214d0fb03d40effa2f0e557016
MauriChamorro <mmchamoo@gmail.com>
2026-04-17 09:53:30 -0300

    static categories repository

---
+++ Models/Repositories/StaticCategoriesRepositories.cs
@@ -0,0 +1,45 @@
+namespace WebAppMVC.Models.Repositories;
+
+public static class StaticCategoriesRepositories
+{
+    private static List<Category> _categories = new List<Category>()
+    {
+        
+        new Category { Id = 1, Name = "Rolls de Salmon", Description = "Clasicos" },
+        new Category { Id = 2, Name = "Rolls de Pescados y Mariscos", Description = "All fish" },
+        new Category { Id = 3, Name = "Rolls Cocidos", Description = "Proteinas cocidas" }
+    };
+    
+    public static void AddCategory(string name, string description)
+    {
+        var maxId = _categories.Max(x => x.Id);
+        _categories.Add(new Category{ Id = maxId + 1, Name = name, Description = description });
+    }
+
+    public static Category[] GetCategories() => _categories.ToArray();
+
+    public static Category? GetCategoryById(int id)
+    {
+        var cat = _categories.FirstOrDefault(x => x.Id == id);
+        return cat != null ? 
+            new Category { Id = cat.Id, Name = cat.Name, Description = cat.Description } 
+            : null;
+    }
+
+    public static void UpdateCategory(Category catUpdated)
+    {
+        var cat = _categories.FirstOrDefault(x => x.Id == catUpdated.Id);
+        if (cat != null)
+        {
+            cat.Name = catUpdated.Name;
+            cat.Description = catUpdated.Description;
+        }
+    }
+
+    public static void DeleteCategory(int id)
+    {
+        var cat = _categories.FirstOrDefault(x => x.Id == id);
+        if (cat != null)
+            _categories.Remove(cat);
+    }
+}
