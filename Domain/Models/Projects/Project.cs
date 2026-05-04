e6453268e0b3ff09caea7762471198eb34eb6e54
MauriChamorro <mmchamoo@gmail.com>
2026-05-03 21:27:41 -0300

    proyect state construction

--- Domain/Models/Projects/Project.cs
+++ Domain/Models/Projects/Project.cs
@@ -4,12 +4,12 @@ public class Project
 {
     public int Id { get; set; }
     public string Title { get; set; }
-    public string ArticlesCount { get; set; }
+    public string Articles { get; set; }
     public string Fundaments { get; set; }
     public string Summary { get; set; }
+    public List<ProjectTag> Tags { get; set; }
     public int ProjectStateId { get; set; }
     public ProjectState State { get; set; }
     public int LawmakerId { get; set; }
     public Lawmaker Lawmaker { get; set; }
-    public List<ProjectTag> Tags { get; set; }
 }
