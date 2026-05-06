87519dcbe2436dcc8bc628245073f2322d6466fb
MauriChamorro <mmchamoo@gmail.com>
2026-05-06 14:40:36 -0300

    basic simulation flow

--- Controllers/SimulationController.cs
+++ Controllers/SimulationController.cs
@@ -2,10 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebAppMVC.Controllers;
 
-public class SimulationController: Controller
+public class SimulationController : Controller
 {
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpPost]
+    [Route("/AssignCommissions")]
+    public IActionResult AssignCommissions()
+    {
+        return RedirectToAction("Index", "Home");
     }
 }
