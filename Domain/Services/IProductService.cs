a97744efc0ca07b8a918d41f1272003997b04852
MauriChamorro <mmchamoo@gmail.com>
2026-05-11 19:53:31 -0300

    deleted older models

--- Domain/Services/IProductService.cs
+++
@@ -1,9 +0,0 @@
-using WebAppMVC.ViewModels;
-
-namespace WebAppMVC.Domain.Services;
-
-public interface IProductService
-{
-    void UpdateCategoriesFor(ProductViewModel productVm);
-    void UpdateProduct(ProductViewModel productVm);
-}
