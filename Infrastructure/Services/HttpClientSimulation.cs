f58abb0aca03ea9c722f67f7f02e13d199707221
MauriChamorro <mmchamoo@gmail.com>
2026-06-12 17:14:24 -0300

    wip - refactor HttpClientSimulaiton Service

--- Infrastructure/Services/HttpClientSimulation.cs
+++ Infrastructure/Services/HttpClientSimulation.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Mime;
 using System.Text;
 using WebAppMVC.Authorization;
@@ -18,20 +19,36 @@ public class HttpClientSimulation : ISimulationServices
         _httpClient.BaseAddress = new Uri("http://localhost:5008/api/Simulation/");
     }
 
-    public async Task<string> AssignCommissionsFor(int projectId)
+    private async Task<HttpResponseMessage> SendPost(string url, ByteArrayContent? content = null)
     {
-        var validationResult = RequestValidations();
-        if (validationResult != null)
-            return validationResult;
-        
+        var messageValidation = RequestValidations();
+        if (messageValidation != null)
+        {
+            var badRequest = new HttpResponseMessage(statusCode: HttpStatusCode.BadRequest);
+            badRequest.Content = StringContent(messageValidation);
+            return  badRequest;
+        }
+
         var cookie = _httpContextAccessor.HttpContext!.Request.Cookies[ExpedientesAuthValues.CookieName];
         _httpClient.DefaultRequestHeaders.Add("Cookie", $"{ExpedientesAuthValues.CookieName}={cookie}");
-        var content = new StringContent("", Encoding.UTF8, MediaTypeNames.Application.Json);
-        var response = await _httpClient.PostAsync($"AssignCommissions/{projectId}", content);
-        var result = await response.Content.ReadAsStringAsync();
+        content ??= EmptyContent();
+        return await _httpClient.PostAsync(url, content);
+    }
+
+    private ByteArrayContent EmptyContent() =>
+        new StringContent("", Encoding.UTF8, MediaTypeNames.Application.Json);
+
+    private ByteArrayContent StringContent(string contentAsString) =>
+        new StringContent(contentAsString, Encoding.UTF8, MediaTypeNames.Application.Json);
+
+    public async Task<string> AssignCommissionsFor(int projectId)
+    {
+        var url = $"AssignCommissions/{projectId}";
+        var response = await SendPost(url);
+        var resultContent = await response.Content.ReadAsStringAsync();
         if (response.IsSuccessStatusCode)
-            result = "Comisiones Asignadas";
-        return result;
+            resultContent = "Comisiones Asignadas";
+        return resultContent;
     }
 
     public async Task<string> DoNextReferringRandomly(int projectId)
@@ -55,11 +72,13 @@ public class HttpClientSimulation : ISimulationServices
         var validationResult = RequestValidations();
         if (validationResult != null)
             return validationResult;
-        
+
         var cookie = _httpContextAccessor.HttpContext!.Request.Cookies[ExpedientesAuthValues.CookieName];
         _httpClient.DefaultRequestHeaders.Add("Cookie", $"{ExpedientesAuthValues.CookieName}={cookie}");
         var content = new StringContent("", Encoding.UTF8, MediaTypeNames.Application.Json);
-        var response = await _httpClient.PostAsync($"EditReferral/{projectId}?commissionId={commissionId}&referralState={(int)state}", content);
+        var response =
+            await _httpClient.PostAsync(
+                $"EditReferral/{projectId}?commissionId={commissionId}&referralState={(int)state}", content);
         var result = await response.Content.ReadAsStringAsync();
         if (response.IsSuccessStatusCode)
             result = "Giro guardado correctamente";
@@ -71,7 +90,7 @@ public class HttpClientSimulation : ISimulationServices
         var validationResult = RequestValidations();
         if (validationResult != null)
             return validationResult;
-        
+
         var cookie = _httpContextAccessor.HttpContext!.Request.Cookies[ExpedientesAuthValues.CookieName];
         _httpClient.DefaultRequestHeaders.Add("Cookie", $"{ExpedientesAuthValues.CookieName}={cookie}");
         var content = new StringContent("", Encoding.UTF8, MediaTypeNames.Application.Json);
@@ -82,10 +101,10 @@ public class HttpClientSimulation : ISimulationServices
 
     public async Task<string> DoSession(int projectId, int sessionResult)
     {
-        var validationResult = RequestValidations();
-        if (validationResult != null)
-            return validationResult;
-        
+        var messageValidation = RequestValidations();
+        if (messageValidation != null)
+            return messageValidation;
+
         var cookie = _httpContextAccessor.HttpContext!.Request.Cookies[ExpedientesAuthValues.CookieName];
         _httpClient.DefaultRequestHeaders.Add("Cookie", $"{ExpedientesAuthValues.CookieName}={cookie}");
         var content = new StringContent("", Encoding.UTF8, MediaTypeNames.Application.Json);
