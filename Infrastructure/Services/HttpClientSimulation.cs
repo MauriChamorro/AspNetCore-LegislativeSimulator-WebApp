84197e08bf1c3765ec8d8ee14a95ae803824f2a5
MauriChamorro <mmchamoo@gmail.com>
2026-06-12 17:26:09 -0300

    post request refactor in simulation service finished

--- Infrastructure/Services/HttpClientSimulation.cs
+++ Infrastructure/Services/HttpClientSimulation.cs
@@ -19,6 +19,52 @@ public class HttpClientSimulation : ISimulationServices
         _httpClient.BaseAddress = new Uri("http://localhost:5008/api/Simulation/");
     }
 
+    public async Task<string> AssignCommissionsFor(int projectId)
+    {
+        var url = $"AssignCommissions/{projectId}";
+        var response = await SendPost(url);
+        var resultContent = await response.Content.ReadAsStringAsync();
+        if (response.IsSuccessStatusCode)
+            resultContent = "Comisiones Asignadas";
+        return resultContent;
+    }
+
+    public async Task<string> DoNextReferringRandomly(int projectId)
+    {
+        var url = $"DoNextReferringRandomly/{projectId}";
+        var response = await SendPost(url);
+        var resultContent = await response.Content.ReadAsStringAsync();
+        if (response.IsSuccessStatusCode)
+            resultContent = "Giro aleatorio realizado";
+        return resultContent;
+    }
+
+    public async Task<string> EditReferral(int projectId, int commissionId, ReferralState state)
+    {
+        var url = $"EditReferral/{projectId}?commissionId={commissionId}&referralState={(int)state}";
+        var response = await SendPost(url);
+        var resultContent = await response.Content.ReadAsStringAsync();
+        if (response.IsSuccessStatusCode)
+            resultContent = "Giro guardado correctamente";
+        return resultContent;
+    }
+
+    public async Task<string> DoSessionRandomly(int projectId)
+    {
+        var url = $"DoSessionRandomly/{projectId}";
+        var response = await SendPost(url);
+        var resultContent = await response.Content.ReadAsStringAsync();
+        return resultContent;
+    }
+
+    public async Task<string> DoSession(int projectId, int sessionResult)
+    {
+        var url = $"DoSession/{projectId}?result={sessionResult}";
+        var response = await SendPost(url);
+        var resultContent = await response.Content.ReadAsStringAsync();
+        return resultContent;
+    }
+
     private async Task<HttpResponseMessage> SendPost(string url, ByteArrayContent? content = null)
     {
         var messageValidation = RequestValidations();
@@ -26,7 +72,7 @@ public class HttpClientSimulation : ISimulationServices
         {
             var badRequest = new HttpResponseMessage(statusCode: HttpStatusCode.BadRequest);
             badRequest.Content = StringContent(messageValidation);
-            return  badRequest;
+            return badRequest;
         }
 
         var cookie = _httpContextAccessor.HttpContext!.Request.Cookies[ExpedientesAuthValues.CookieName];
@@ -40,78 +86,6 @@ public class HttpClientSimulation : ISimulationServices
 
     private ByteArrayContent StringContent(string contentAsString) =>
         new StringContent(contentAsString, Encoding.UTF8, MediaTypeNames.Application.Json);
-
-    public async Task<string> AssignCommissionsFor(int projectId)
-    {
-        var url = $"AssignCommissions/{projectId}";
-        var response = await SendPost(url);
-        var resultContent = await response.Content.ReadAsStringAsync();
-        if (response.IsSuccessStatusCode)
-            resultContent = "Comisiones Asignadas";
-        return resultContent;
-    }
-
-    public async Task<string> DoNextReferringRandomly(int projectId)
-    {
-        var validationResult = RequestValidations();
-        if (validationResult != null)
-            return validationResult;
-
-        var cookie = _httpContextAccessor.HttpContext!.Request.Cookies[ExpedientesAuthValues.CookieName];
-        _httpClient.DefaultRequestHeaders.Add("Cookie", $"{ExpedientesAuthValues.CookieName}={cookie}");
-        var content = new StringContent("", Encoding.UTF8, MediaTypeNames.Application.Json);
-        var response = await _httpClient.PostAsync($"DoNextReferringRandomly/{projectId}", content);
-        var result = await response.Content.ReadAsStringAsync();
-        if (response.IsSuccessStatusCode)
-            result = "Giro aleatorio realizado";
-        return result;
-    }
-
-    public async Task<string> EditReferral(int projectId, int commissionId, ReferralState state)
-    {
-        var validationResult = RequestValidations();
-        if (validationResult != null)
-            return validationResult;
-
-        var cookie = _httpContextAccessor.HttpContext!.Request.Cookies[ExpedientesAuthValues.CookieName];
-        _httpClient.DefaultRequestHeaders.Add("Cookie", $"{ExpedientesAuthValues.CookieName}={cookie}");
-        var content = new StringContent("", Encoding.UTF8, MediaTypeNames.Application.Json);
-        var response =
-            await _httpClient.PostAsync(
-                $"EditReferral/{projectId}?commissionId={commissionId}&referralState={(int)state}", content);
-        var result = await response.Content.ReadAsStringAsync();
-        if (response.IsSuccessStatusCode)
-            result = "Giro guardado correctamente";
-        return result;
-    }
-
-    public async Task<string> DoSessionRandomly(int projectId)
-    {
-        var validationResult = RequestValidations();
-        if (validationResult != null)
-            return validationResult;
-
-        var cookie = _httpContextAccessor.HttpContext!.Request.Cookies[ExpedientesAuthValues.CookieName];
-        _httpClient.DefaultRequestHeaders.Add("Cookie", $"{ExpedientesAuthValues.CookieName}={cookie}");
-        var content = new StringContent("", Encoding.UTF8, MediaTypeNames.Application.Json);
-        var response = await _httpClient.PostAsync($"DoSessionRandomly/{projectId}", content);
-        var result = await response.Content.ReadAsStringAsync();
-        return result;
-    }
-
-    public async Task<string> DoSession(int projectId, int sessionResult)
-    {
-        var messageValidation = RequestValidations();
-        if (messageValidation != null)
-            return messageValidation;
-
-        var cookie = _httpContextAccessor.HttpContext!.Request.Cookies[ExpedientesAuthValues.CookieName];
-        _httpClient.DefaultRequestHeaders.Add("Cookie", $"{ExpedientesAuthValues.CookieName}={cookie}");
-        var content = new StringContent("", Encoding.UTF8, MediaTypeNames.Application.Json);
-        var response = await _httpClient.PostAsync($"DoSession/{projectId}?result={sessionResult}", content);
-        var result = await response.Content.ReadAsStringAsync();
-        return result;
-    }
 
     private string? RequestValidations()
     {
