7b7e78ca3311352e61f4c76f296187d95a6545a1
MauriChamorro <mmchamoo@gmail.com>
2026-06-15 15:57:50 -0300

    confirmation modal refactor

--- ViewModels/NotificationViewModel.cs
+++ ViewModels/NotificationViewModel.cs
@@ -5,5 +5,4 @@ public class NotificationViewModel
     public string UserIdentifier { get; set; }
     public string Title { get; set; }
     public string Message { get; set; }
-    public string Type { get; set; }
 }
